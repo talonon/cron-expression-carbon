595be546c0928a1145a4417152c6bb31a15de552
Andy <andy@griffonco.com>
2015-11-07 23:44:08 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cron-expression-carbon
-Mashup between cron-expression (https://github.com/mtdowling/cron-expression) and carbon (https://github.com/briannesbitt/carbon) for Laravel 5.
+Mashup between cron-expression (https://github.com/mtdowling/cron-expression) and carbon (https://github.com/briannesbitt/carbon) for Laravel 4/5.
---
+++ src/CronExpression.php
@@ -0,0 +1,40 @@
+<?php namespace Talonon\Cron;
+use Carbon\Carbon;
+use Cron\CronExpression as CronExpressionBase;
+
+/**
+ * @author Andy Muth <andy@griffonco.com>
+ * @method array|Carbon[] getMultipleRunDates(int $total, string $currentTime = 'now', bool $invert = false, bool $allowCurrentDate = false)
+ */
+class CronExpression extends CronExpressionBase {
+  /**
+   * @param string $currentTime
+   * @param int $nth
+   * @param bool|false $allowCurrentDate
+   * @return Carbon
+   */
+  public function getNextRunDate($currentTime = 'now', $nth = 0, $allowCurrentDate = false) {
+    return Carbon::instance(parent::getNextRunDate($currentTime, $nth, $allowCurrentDate));
+  }
+
+  /**
+   * @param string $currentTime
+   * @param int $nth
+   * @param bool|false $allowCurrentDate
+   * @return Carbon
+   */
+  public function getPreviousRunDate($currentTime = 'now', $nth = 0, $allowCurrentDate = false) {
+    return Carbon::instance(parent::getPreviousRunDate($currentTime, $nth, $allowCurrentDate));
+  }
+
+  /**
+   * @param null $currentTime
+   * @param int $nth
+   * @param bool|false $invert
+   * @param bool|false $allowCurrentDate
+   * @return Carbon
+   */
+  protected function getRunDate($currentTime = null, $nth = 0, $invert = false, $allowCurrentDate = false) {
+    return Carbon::instance(parent::getRunDate($currentTime, $nth, $invert, $allowCurrentDate));
+  }
+}
